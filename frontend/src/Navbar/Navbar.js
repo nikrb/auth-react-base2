2d12ab09e554f8b43bab2066e7865c3171acc023
nikrb <nik_rb@yahoo.com>
2018-02-14 19:51:10 +0000

    simplify nav styling

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -10,72 +10,68 @@ class navbar extends Component {
     user: PropTypes.object.isRequired,
     logout: PropTypes.func.isRequired
   };
-
-  render() {
-    return this.getBar();
-  }
-
-  getBar() {
+  render = () => {
     const {name} = this.props.user;
     if (name) { // authenticated
       return (
-        <div>
-          <ul className={css(styles.container)}>
-            <li className={css(styles.titleelement)}>
-              <Link className={css(styles.links)} to="/">Home</Link>
-            </li>
-            <li className={css(styles.titleelement)}>
-              <Link className={css(styles.links)} to="/profile">Profile</Link>
-            </li>
-            <li className={css(styles.navelement)}
-              onClick={() => this.props.logout()}>
-              <Link className={css(styles.links)} to="/logout">Logout</Link>
-            </li>
-            <li className={css(styles.navelement)} >
-              <span className={css(styles.links)}>Hi {name}</span>
+        <div className={css([styles.flexContainer, styles.blueBg])}>
+          <ul className={css([styles.nav, styles.flexItem, styles.flexStart])}>
+            <li><Link className={css(styles.link)} to="/">Home</Link></li>
+            <li><Link className={css(styles.link)} to="/profile">Profile</Link></li>
+          </ul>
+          <ul className={css([styles.nav, styles.flexContainer, styles.flexEnd])} >
+            <li className={css(styles.link)}>Hi {name}</li>
+            <li onClick={this.props.logout}>
+              <Link className={css(styles.link)} to="/logout">Logout</Link>
             </li>
           </ul>
         </div>
       );
     } else { // not authenticated
       return (
-        <div>
-          <ul className={css(styles.container)}>
-            <li className={css(styles.titleelement)}><Link className={css(styles.links)} to="/">Home</Link></li>
-            <li className={css(styles.navelement)}><Link className={css(styles.links)} to="/login">Login</Link></li>
-            <li className={css(styles.navelement)}><Link className={css(styles.links)} to="/register">Register</Link></li>
+        <div className={css([styles.flexContainer, styles.blueBg])}>
+          <ul className={css([styles.nav, styles.flexItem, styles.flexStart])}>
+            <li><Link className={css(styles.link)} to="/">Home</Link></li>
+          </ul>
+          <ul className={css([styles.nav, styles.flexContainer, styles.flexEnd])} >
+            <li><Link className={css(styles.link)} to="/register">Register</Link></li>
+            <li><Link className={css(styles.link)} to="/login">Login</Link></li>
           </ul>
         </div>
       );
     }
-  }
-
+  };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    listStyleType: 'none',
-    margin: 0,
-    fontSize: '1.1rem',
-    padding: '0.8rem 1rem',
-    overflow: 'hidden',
-    backgroundColor: '#333',
-    background: 'linear-gradient(45deg, rgba(49,130,204,0.95), rgba(132,183,230,1.0))'
+  flexContainer: {
+    display: "flex"
   },
-  navelement: {
-    float: 'right',
+  flexItem: {
+    flex: "1"
   },
-  titleelement: {
-    float: 'left',
+  flexStart: {
+    justifyContent: "flex-start"
   },
-  links: {
-    display: 'block',
-    color: 'white',
-    textAlign: 'center',
-    padding: '14px 16px',
-    textDecoration: 'none',
-    cursor: 'pointer',
+  flexEnd: {
+    justifyContent: "flex-end"
   },
+  blueBg: {
+    background: "linear-gradient(45deg, rgba(49,130,204,0.95), rgba(132,183,230,1.0))"
+  },
+  nav: {
+    listStyle: "none",
+    fontSize: "1.2rem"
+  },
+  link: {
+    textDecoration: "none",
+    display: "block",
+    padding: "0 1rem",
+    color: "white",
+    ":hover": {
+      color: "yellow"
+    }
+  }
 });
 
 
